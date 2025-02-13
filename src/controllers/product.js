d92674b9f2d25bb3be44853f3578a65a1fb690d5
Manh Linh <mlinhalone@gmail.com>
2025-02-13 10:18:46 +0700

    finish CRUD with data fake

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -36,3 +36,54 @@ export const getProductById = (req, res) => {
     return res.status(400).json({ error: error.message });
   }
 };
+
+export const removeProduct = (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = data.find((item) => item.id === +id);
+    if (!product)
+      return res.status(404).json({ message: "Sản phẩm không tồn tại" });
+    const newData = data.filter((item) => item.id !== +id);
+    return res
+      .status(200)
+      .json({ message: "Xóa sản phẩm thành công", newData });
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+};
+
+export const updateProduct = (req, res) => {
+  try {
+    const { id } = req.params;
+    const { body } = req;
+    const product = data.find((item) => item.id === +id);
+    if (!product)
+      return res.status(404).json({ message: "Sản phẩm không tồn tại" });
+    data.map((item) => (item.id === +id ? { id: +id, ...body } : item));
+    return res.status(200).json({
+      message: "Cập nhật sản phẩm thành công",
+      product: { id: +id, ...body },
+    });
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+};
+
+export const createProduct = (req, res) => {
+  try {
+    const { id, name, price } = req.body;
+    if (!id || !name || !price) {
+      return res.status(400).json({ message: "Thiếu thông tin sản phẩm" });
+    }
+    const existProduct = data.find((item) => item.id === id);
+    if (existProduct)
+      return res.status(400).json({ message: "Sản phẩm đã tồn tại" });
+    const newProduct = { id, name, price };
+    data.push(newProduct);
+    return res
+      .status(201)
+      .json({ message: "Thêm sản phẩm thành công", newProduct });
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+};
--- src/routers/product.js
+++ src/routers/product.js
@@ -1,8 +1,17 @@
 import { Router } from "express";
-import { getAllProducts, getProductById } from "../controllers/product";
+import {
+  createProduct,
+  getAllProducts,
+  getProductById,
+  removeProduct,
+  updateProduct,
+} from "../controllers/product";
 
 const router = Router();
 router.get("/products", getAllProducts);
+router.post("/products", createProduct);
 router.get("/products/:id", getProductById);
+router.delete("/products/:id", removeProduct);
+router.put("/products/:id", updateProduct);
 
 export default router;
